cf043707ef2cfa364bee35beee2eff8408294503
nighthawkbravo <lucas@aggati.com>
2021-10-01 17:02:01 +0200

    Added more GUI and some functions

--- Matcher/MainWindow.xaml.cs
+++ Matcher/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
+using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
+using System.IO;
+
 namespace Matcher
 {
     /// <summary>
@@ -20,9 +25,42 @@ namespace Matcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string PlayersList;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void UploadButton(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a text file";
+            op.Filter = "Text file (*.txt)|*.txt;";
+            if (op.ShowDialog() == System.Windows.Forms.DialogResult.OK) PlayersList = File.ReadAllText(op.FileName);
+
+            //StringBuilder sb;
+            //StringReader sr = new StringReader(PlayersList);
+
+
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClose(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
