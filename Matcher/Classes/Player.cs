b9357adfed5d866548b2d653e3b2c4dbbbec1be7
nighthawkbravo <lucas@aggati.com>
2021-10-02 12:33:30 +0200

    Added a Player class

---
+++ Matcher/Classes/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matcher.Classes
+{
+    public class Player
+    {
+        private string firstName;
+        private string lastName;
+        private string faction;
+
+        public List<Player> opponents;
+
+        public Player(string fn, string ln, string f)
+        {
+            firstName = fn;
+            lastName = ln;
+            faction = f;
+        }
+
+        public void SetOpponents(int op)
+        {
+            opponents = new List<Player>(op);
+        }
+
+        public bool TryAssigning(Player p)
+        {
+            if (!opponents.Contains(p) && opponents.Count < opponents.Capacity)
+            {
+                opponents.Add(p);
+                return true;
+            }
+            else return false;
+        }
+
+        public string Print()
+        {
+            return $"{firstName} {lastName} ({faction})";
+        }
+
+        public string PrintWithOpponents()
+        {
+            string s = $"{firstName} {lastName} ({faction})\n";
+            int count = 1;
+            foreach(var p in opponents)
+            {
+                s += $"\t\tRound #{count} - {p.Print()}\n";
+                count++;
+            }
+            s += "\n";
+            return s;
+        }
+    }
+}
